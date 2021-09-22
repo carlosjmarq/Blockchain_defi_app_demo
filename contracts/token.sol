c46c007cfb41bda347d7c805bf905513f3050629
Carlos J Marquez <carlosjmarqmail.com>
2021-09-22 08:24:27 -0400

    Añadiendo truffle y ganache al proyecto

--- contracts/token.sol
+++ contracts/token.sol
@@ -4,19 +4,19 @@ pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Corchea_Token is ERC20 {
-	// Local Variables of the contract
+	/**Local Variables of the contract */
 	address public minter; 
 
-	// Events (Console Logs) for the contract
+	/**Events (Console Logs) for the contract */
 	event MinterChanged(address indexed from, address to);
 
-	//Constructor (not necessary in abstracts contracts)
+	/**Constructor (not necessary in abstracts contracts)*/
 	constructor() payable ERC20('Corchea', 'CCH') {  // ('NAME','SYMBOL')
 		minter = msg.sender;
 
 	}
 
-	//methods of the contracts
+	/**methods of the contracts*/
 	function passMinterRole(address dBank) public returns(bool) {
 		require(msg.sender == minter, "KABOOM: The sender does not have minter role");
 		minter = dBank;
